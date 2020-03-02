55faa23fb9a8eb8ba71e55302a6b16a8c8edd4b6
Tanvir Sarker <tanvir603@gmail.com>
2020-03-02 17:45:48 +0600

    problem 5

---
+++ problem-5/cmake-build-debug/CMakeFiles/problem_5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem_5.dir/main.c.obj"
+  "libproblem_5.dll.a"
+  "problem_5.exe"
+  "problem_5.exe.manifest"
+  "problem_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/problem_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ problem-5/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    for (int i = 1; i <= 10; ++i) {
+
+        //if (i == 1) {
+            for (int j = 0; j < 10; ++j) {
+                printf("* ");
+            }
+        //}
+
+        printf("*\n");
+
+        
+
+    }
+
+    return 0;
+}
