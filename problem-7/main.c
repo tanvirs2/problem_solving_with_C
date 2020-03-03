5b5713a053c0692689301c41df1ecd951a1ff1a9
unknown <tanvir603@gmail.com>
2020-03-03 22:03:12 +0600

    problem-7 some codeing

--- problem-7/main.c
+++ problem-7/main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
-    int T;
+    int T, n_size;
+    char n[] = "10 36 669";
 
-    scanf("%d", &T);
-    
+    n_size = strlen(n);
 
-    printf("%d\n", T);
+    for (int i = 0; i < n_size; ++i) {
+
+        //printf("%c", n[i]);
+
+
+        if (n[i] == ' ') {
+            printf(" 1 ");
+        }
+    }
+
+    //printf("%d", strlen(n));
+
+    /*scanf("%d", &T);
+
+
+    for (int i = 0; i < T; ++i) {
+
+    }
+
+    printf("%d\n", T);*/
     return 0;
 }
