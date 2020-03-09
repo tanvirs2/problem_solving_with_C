0ed0f16413e3b5d7abe5602c5456a8d8f93b29da
unknown <tanvir603@gmail.com>
2020-03-09 22:09:08 +0600

    dont know

--- problem-7/main.c
+++ problem-7/main.c
@@ -3,30 +3,36 @@
 
 int main() {
 
-    int T, n_size;
-    char n[] = "10 36 669";
+    int T, n_size, sp=0, arrIndex[100], i, yy;
+
+    char n[] = "10 36       669";
 
     n_size = strlen(n);
 
-    for (int i = 0; i < n_size; ++i) {
-
-        //printf("%c", n[i]);
+    for (i = 0; i < n_size; ++i) {
 
 
         if (n[i] == ' ') {
-            printf(" 1 ");
+
+            arrIndex[sp] = i;
+
+            sp++;
         }
-    }
-
-    //printf("%d", strlen(n));
-
-    /*scanf("%d", &T);
-
-
-    for (int i = 0; i < T; ++i) {
 
     }
 
-    printf("%d\n", T);*/
+
+    for (int j = 0; j < 100; ++j) {
+
+
+
+    }
+
+    //printf("%d", arrIndex[0]);
+
+
+    //printf("Length of array: %d\n", (int)( sizeof(arrIndex) / sizeof(arrIndex[0]) ));
+
+
     return 0;
 }
