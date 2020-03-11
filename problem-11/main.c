682db65144e8e1b9b91460b36170eeb7f9fa82c4
Tanvir Sarker <tanvir603@gmail.com>
2020-03-11 16:23:29 +0600

    problem-11 done

--- problem-11/main.c
+++ problem-11/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int T;
+    int n, fact=1;
+
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; ++i) {
+
+        scanf("%d", &n);
+
+        for (int j = 1; j <=n; ++j) {
+            fact = fact * j;
+        }
+        printf("%d\n", fact);
+        fact = 1;
+    }
+
+
     return 0;
 }
