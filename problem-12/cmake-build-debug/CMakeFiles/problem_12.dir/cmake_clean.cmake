1e12784cfcc00c8ef62a016457886d923d9c734a
Tanvir Sarker <tanvir603@gmail.com>
2020-03-11 17:47:21 +0600

    problem-12 init

---
+++ problem-12/cmake-build-debug/CMakeFiles/problem_12.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem_12.dir/main.c.obj"
+  "libproblem_12.dll.a"
+  "problem_12.exe"
+  "problem_12.exe.manifest"
+  "problem_12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/problem_12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ problem-12/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    long  tt = 8000000001;
+
+
+
+    printf("%li\n", tt);
+    return 0;
+}
