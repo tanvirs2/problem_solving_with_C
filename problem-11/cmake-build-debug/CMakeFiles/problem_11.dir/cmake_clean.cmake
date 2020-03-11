c3771c2df92cd3ad439e7e8c4f407e7cbc33b921
Tanvir Sarker <tanvir603@gmail.com>
2020-03-11 13:30:40 +0600

    problem-11 init

---
+++ problem-11/cmake-build-debug/CMakeFiles/problem_11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem_11.dir/main.c.obj"
+  "libproblem_11.dll.a"
+  "problem_11.exe"
+  "problem_11.exe.manifest"
+  "problem_11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/problem_11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ problem-11/cmake-build-debug/CMakeFiles/problem_11.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for problem_11.
+# This may be replaced when dependencies are built.
