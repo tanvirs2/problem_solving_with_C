5d54fcc7bb4794f2a4539ac1d8c249f1b0a4db54
Tanvir Sarker <tanvir603@gmail.com>
2020-03-11 12:22:03 +0600

    prblem-10 done

--- problem-10/main.c
+++ problem-10/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int totalOver = 50, overDone, runDone, ballDone, hasBall, avgRun;
+    int T, totalOver = 50;
+    float overDone, runDone, ballDone, hasBall, avgRun, opponentRun, needRunForWin, needRunInOver;
 
-    scanf("%d %d", &runDone, &hasBall);
+    scanf("%d", &T);
 
-    ballDone = (totalOver * 6) - hasBall;
-    overDone = ballDone / 6;
-    avgRun = runDone / overDone;
-
-    printf("%d", avgRun);
+    while (T--) {
+        scanf("%f %f %f", &opponentRun, &runDone, &hasBall);
+        needRunForWin = 1 + (opponentRun - runDone);
+        needRunInOver = needRunForWin / (hasBall / 6);
+        ballDone = (totalOver * 6) - hasBall;
+        overDone = ballDone / 6;
+        avgRun = runDone / overDone;
+        if (opponentRun > runDone) {
+            printf("%.2f %.2f\n", avgRun, needRunInOver);
+        } else {
+            printf("%.2f %.2f\n", avgRun, 0.00);
+        }
+    }
 
     return 0;
 }
