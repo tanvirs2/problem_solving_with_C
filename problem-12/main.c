9ce4c8a757bbcca10ad55e24c24b21acd0891af6
Tanvir Sarker <tanvir603@gmail.com>
2020-03-16 17:38:37 +0600

    problem-12 cont

--- problem-12/main.c
+++ problem-12/main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    long  tt = 8000000001;
+
+    long long n, store = 1, count=0;
+
+    n = 20;
+
+    for (int i = 1; i <= n; ++i) {
+
+        store = store * i;
+
+    }
+
+    while (store % 10 == 0) {
+        count++;
+        store = store / 10;
+        printf(" %lld ", store);
+    }
 
 
+    printf("%lld", count);
 
-    printf("%li\n", tt);
     return 0;
 }
